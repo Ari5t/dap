0d25d770b9bc3ee98a61630a70f68cead81853e0
Yaroslav Burianov <yburyanov@gmail.com>
2024-10-26 17:42:21 +0300

    Update: added showing messages

--- src/pages/scripts/index.js
+++ src/pages/scripts/index.js
@@ -1,6 +1,39 @@
-const func = async () => {
-  const response = await window.storages.messages()
-  console.log(response) // prints out 'pong'
-}
+// <li>
+//   <p class="title">Message</p>
+//   <div class="home__buttons-group">
+//     <a class="button button-edit">Edit</a>
+//     <a class="button button-delete">Delete</a>
+//   </div>
+// </li>
 
-func()
+;(async function () {
+  const messageList = document.getElementById('message-list')
+  const messages = await window.storages.messages()
+
+  messages.forEach((element) => {
+    const message = document.createElement('li')
+
+    const messageTitle = document.createElement('p')
+    messageTitle.classList.add('title')
+    messageTitle.innerText = element.name
+
+    const actionButtons = document.createElement('div')
+    actionButtons.classList.add('home__buttons-group')
+
+    const editButton = document.createElement('a')
+    editButton.classList.add('button', 'button-edit')
+    editButton.innerText = 'Edit'
+
+    const deleteButton = document.createElement('a')
+    deleteButton.classList.add('button', 'button-delete')
+    deleteButton.innerText = 'Delete'
+
+    actionButtons.appendChild(editButton)
+    actionButtons.appendChild(deleteButton)
+
+    message.appendChild(messageTitle)
+    message.appendChild(actionButtons)
+
+    messageList.appendChild(message)
+  })
+})()
